6f87b03d0ee7383c795f8eeda1476c6f6ef6faf9
jessssouza <jessica-soares96@hotmail.com>
2024-06-28 01:26:16 -0300

    tarefa 13 done

---
+++ tarefa_13/mean.js
@@ -0,0 +1,23 @@
+const mean = (num1, num2, num3, num4, num5) => {
+  return (num1 + num2 + num3 + num4 + num5) / 5;
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const inputs = [
+    document.getElementById("num1").value,
+    document.getElementById("num2").value,
+    document.getElementById("num3").value,
+    document.getElementById("num4").value,
+    document.getElementById("num5").value,
+  ];
+
+  const isBlank = inputs.some((num) => !num);
+  if (isBlank) {
+    alert("Preencha todos os campos");
+  } else {
+    const inputsAsNumer = inputs.map((num) => Number(num));
+    alert(`A média é ${mean(...inputsAsNumer)}`);
+  }
+}
